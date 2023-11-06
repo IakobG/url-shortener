9733f3b151a185b85e6783e8d29ac7d2da039206
IakobG <iakob.gujejiani@gmail.com>
2023-11-06 17:42:10 +0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const url = require('./urls')
+require('dotenv').config()
 
 const app = express()
 app.set('view engine', 'ejs')
